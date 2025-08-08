5ca4a13475de077090e836e899cb8f78946d3033
Pedro <stabilepedro010403@gmail.com>
2025-08-07 23:38:59 -0300

    feat: adding Entity Configuration to Location and Event

---
+++ Aliance.Infrastructure/EntitiesConfiguration/EventConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Aliance.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliance.Infrastructure.EntitiesConfiguration;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.ToTable("Event");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(150);
+
+        builder.Property(e => e.Description)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(e => e.Date)
+            .IsRequired();
+
+        builder.Property(e => e.Cost)
+            .IsRequired()
+            .HasColumnType("decimal(18,2)");
+
+        builder.HasOne(e => e.Location)
+            .WithMany()
+            .HasForeignKey(e => e.LocationId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
