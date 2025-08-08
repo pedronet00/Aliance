8fd7be6dd81f5f37537a0ec501c70d9d9f6e0df1
Pedro <stabilepedro010403@gmail.com>
2025-08-08 19:24:43 -0300

    feat: creating DTO's to prepare my application to receive the JWT's authentication

---
+++ Aliance.Application/DTOs/RegisterDTO.cs
@@ -0,0 +1,17 @@
+﻿using Aliance.Domain.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aliance.Application.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = DataAnnotationMessages.REQUIRED)]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = DataAnnotationMessages.REQUIRED)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = DataAnnotationMessages.REQUIRED)]
+        public string? Password { get; set; }
+    }
+}
