776bd254036bfe1d2ad13460ba6b483e726b45e3
Pedro <stabilepedro010403@gmail.com>
2025-08-09 13:48:55 -0300

    fix: changing login method. Instead of logging using user's name, now we log with user's email.

--- Aliance.Application/DTOs/LoginDTO.cs
+++ Aliance.Application/DTOs/LoginDTO.cs
@@ -6,7 +6,7 @@ namespace Aliance.Application.DTOs;
 public class LoginDTO
 {
     [Required(ErrorMessage = DataAnnotationMessages.REQUIRED)]
-    public string? UserName { get; set; }
+    public string? Email { get; set; }
 
     [Required(ErrorMessage = DataAnnotationMessages.REQUIRED)]
     public string? Password { get; set; }
