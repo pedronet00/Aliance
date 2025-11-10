6361b4751646ae0147d2f825cf15fbf1ecaa6c25
pedronet00 <stabilepedro010403@gmail.com>
2025-11-10 20:55:43 -0300

    feat: updating path to deploy

---
+++ Aliance.API/Controllers/DepartmentMemberController.cs
@@ -0,0 +1,59 @@
+﻿using Aliance.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aliance.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DepartmentMemberController : ControllerBase
+    {
+        private readonly IDepartmentMemberService _service;
+
+        public DepartmentMemberController(IDepartmentMemberService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{departmentGuid:guid}")]
+        public async Task<IActionResult> GetDepartmentMembers(Guid departmentGuid, int pageNumber = 1, int pageSize = 5)
+        {
+            var result = await _service.GetDepartmentMembers(departmentGuid, pageNumber, pageSize);
+            if (result.HasNotifications)
+                return BadRequest(result.Notifications);
+
+            return Ok(result.Result);
+        }
+
+        [HttpPost("{departmentGuid:guid}/member/{memberId}")]
+        public async Task<IActionResult> InsertDepartmentMember(Guid departmentGuid, string memberId)
+        {
+            var result = await _service.InsertDepartmentMember(departmentGuid, memberId);
+            if (result.HasNotifications)
+                return BadRequest(result.Notifications);
+
+            return Ok(result.Result);
+        }
+
+        [HttpDelete("{departmentGuid:guid}/member/{memberId}")]
+        public async Task<IActionResult> DeleteDepartmentMember(Guid departmentGuid, string memberId)
+        {
+            var result = await _service.DeleteDepartmentMember(departmentGuid, memberId);
+            if (result.HasNotifications)
+                return BadRequest(result.Notifications);
+
+            return NoContent();
+        }
+
+        [HttpPatch("{departmentGuid:guid}/member/{memberId}/status")]
+        public async Task<IActionResult> ToggleMemberStatus(Guid departmentGuid, string memberId)
+        {
+            var result = await _service.ToggleMemberStatus(departmentGuid, memberId);
+            if (result.HasNotifications)
+                return BadRequest(result.Notifications);
+
+            return Ok(result.Result);
+        }
+    }
+}
